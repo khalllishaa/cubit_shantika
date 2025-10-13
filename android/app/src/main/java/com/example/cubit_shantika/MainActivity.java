fd08fc3934b52322bed38426ecc05750466619cb
khalisha kaylanasywa <kkaylanasywa>
2025-10-13 10:11:32 +0700

    Initial commit

---
+++ android/app/src/main/java/com/example/cubit_shantika/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.cubit_shantika;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/config/constant.dart
@@ -0,0 +1,4 @@
+class AppConfig {
+  static const String rawgBase = 'https://api.rawg.io/api';
+  static const String apiKey = '40c26eb90f2b42b49874b203ec03ddd8';
+}
